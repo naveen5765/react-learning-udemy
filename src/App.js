0f6b135073af775a5ffeeb113ba7523e5724d6b5
Naveen Anandhan <naveenanandhan@Naveens-MacBook-Pro.local>
2018-08-13 12:35:00 +0200

    Added prettier on precommit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 30 },
+      { name: "Stephanie", age: 26 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I am a React App </h1>
+        <button> Switch Name </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies : Gaming
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
+    /* return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "Hi, I am a React App !!!")
+    ); */
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Person = props => {
+  return (
+    <div>
+      <p>
+        I am {props.name} and i am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export default Person;
